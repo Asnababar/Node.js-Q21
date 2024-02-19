be14e23c031123070ad1e9b8852bf9b1f27d3d11
Asnababar <147834277+Asnababar@users.noreply.github.com>
2024-02-19 19:00:48 +0500

    Node.js Q21

---
+++ Q21.ts
@@ -0,0 +1,4 @@
+// They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+let person : {name:string, lname:string, age:number} = {name: "Zainab", lname: "Babar", age:9}
+console.log(person)
